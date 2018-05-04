3827715eb098b00024310f82747752eba475655d
Abdiel Soto <abdielsba@gmail.com>
2018-05-03 22:04:01 -0500

    Initial Commit

---
+++ Youtube Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Youtube Challenge
+//
+//  Created by Abdiel Soto on 5/3/18.
+//  Copyright © 2018 Applicaster. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
